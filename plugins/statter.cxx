8409957842aff7ea14bd6e1552042c66a352347f
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-02-22 15:35:23 +0500

    Version 0.5
    Alpha version

--- plugins/statter.cxx
+++ plugins/statter.cxx
@@ -1,3 +1,14 @@
 # include <string>
+# include <fstream>
 using namespace std;
 
+struct stats {
+	int home_moved;
+	int pay_moved;
+	int pays_and_moved;
+}
+
+
+void create_file(string file, string all_files) {
+	ofstream file(file.c_str());
+}
