765949df454dc8967bc47f9cd231d5b863c8e6e4
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-03-15 19:10:52 +0500

    Version 0.7
    Alpha version

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from json import loads
 from aiohttp.web import Application, run_app, RouteTableDef, Response, Request, json_response
 from plugins.binder import Binder
+from static import app as subapp
 
 routes = RouteTableDef()
 app = Application()
 binder = Binder()
+
+app.add_subapp('/', subapp)
 
 
 @routes.get('/service/get?method={method}&data={data}')
@@ -32,47 +35,6 @@ async def service_path(request: Request):
 		return json_response(
 			data={'error': f'Method {method} not exist!'}
 		)
-
-
-@routes.get('/scripts/{scriptname}')
-async def get_js(request: Request):
-	data = await binder.get_js(path='scripts/' + str(request.url).split('/')[-1])
-	return Response(
-		status=data['status'],
-		body=data['body'],
-		content_type=data['content_type']
-	)
-
-
-@routes.get('/styles/{stylesheet}')
-async def get_css(request: Request):
-	data = await binder.get_css(path='styles/' + str(request.url).split('/')[-1])
-	return Response(
-		status=data['status'],
-		body=data['body'],
-		content_type=data['content_type']
-	)
-
-
-@routes.get('/header/{stylesheet}')
-async def get_css2(request: Request):
-	data = await binder.get_css(path='styles/' + str(request.url).split('/')[-1])
-	return Response(
-		status=data['status'],
-		body=data['body'],
-		content_type=data['content_type']
-	)
-
-
-@routes.get('/png/{filename}')
-async def get_png(request: Request):
-	data = await binder.get_png(path='png/' + str(request.url).split('/')[-1])
-	return Response(
-		status=data['status'],
-		body=data['body'],
-		content_type=data['content_type']
-	)
-
 
 @routes.get('/')
 async def home_page(request: Request):
---
+++ static.py
@@ -0,0 +1,56 @@
+from aiohttp.web import Response, Application, Request, RouteTableDef
+from plugins.binder import Binder
+
+app = Application()
+routes = RouteTableDef()
+binder = Binder()
+
+@routes.get('/')
+async def main_staticz_handler(request:Request):
+	return Response(status=418)
+
+@routes.get('/scripts/{scriptname}')
+async def get_js(request: Request):
+	data = await binder.get_js(path='scripts/' + str(request.url).split('/')[-1])
+	return Response(
+		status=data['status'],
+		body=data['body'],
+		content_type=data['content_type']
+	)
+
+
+@routes.get('/styles/{stylesheet}')
+async def get_css(request: Request):
+	data = await binder.get_css(path='styles/' + str(request.url).split('/')[-1])
+	return Response(
+		status=data['status'],
+		body=data['body'],
+		content_type=data['content_type']
+	)
+
+
+@routes.get('/header/{stylesheet}')
+async def get_css2(request: Request):
+	data = await binder.get_css(path='styles/' + str(request.url).split('/')[-1])
+	return Response(
+		status=data['status'],
+		body=data['body'],
+		content_type=data['content_type']
+	)
+
+
+@routes.get('/png/{filename}')
+async def get_png(request: Request):
+	data = await binder.get_png(path='png/' + str(request.url).split('/')[-1])
+	return Response(
+		status=data['status'],
+		body=data['body'],
+		content_type=data['content_type']
+	)
+
+app.add_routes(routes)
+
+if __name__ == '__main__':
+	from aiohttp.web import run_app
+
+	run_app(app, host='127.0.0.1', port=80)
